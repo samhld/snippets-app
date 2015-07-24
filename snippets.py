534992f881aa31130790ced6009670519b5fe2df
Sam Dillard <samhdillard@gmail.com>
2015-07-24 00:03:44 +0000

    initial commit

---
+++ snippets.py
@@ -0,0 +1,23 @@
+import logging
+
+#set the log output file, and the log level
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name, snippet):
+    """
+    Store a snippet with an associate name
+    Returns the name and the snippet
+    """
+    
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+    
+def get(name):
+    """
+    Retrieve the snippet with a given name
+    If there is no such snippet...error will be reported to snippets.log
+    Returns the snippet
+    """
+    
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
