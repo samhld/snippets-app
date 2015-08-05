e56fe509f2046436c0bcc07450973efbb64330a2
Sam Dillard <samhdillard@gmail.com>
2015-08-05 02:27:00 +0000

    added get parser

--- snippets.py
+++ snippets.py
@@ -38,15 +38,21 @@ snippet = "A sequence of things - created using []"
 
 
 def main():
-    """main function"""
+    """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    
+
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
-    
-    #subparser for put command
+
+    # Subparser for the put command
     logging.debug("Constructing put subparser")
     put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+    get_parser.add_argument("snippet", help="The snippet text")
     
     arguments = parser.parse_args(sys.argv[1:])
 
